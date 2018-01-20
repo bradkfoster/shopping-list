a10ae58a57d3d243c4c03f93dde68206a2c38701
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-01-20 08:56:58 -1000

    updated

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -20,53 +20,77 @@ describe('everything', function () {
 
       expect(param1).to.be.a("string");
       shoppingListItem.name.should.equal(param1);
-    
+
     });
     it('property should be description', function () {
       expect(param2).to.be.a("string");
       expect(shoppingListItem.description).to.equal(param2);
     });
-    
-    it('property should is_done', function (){
+
+    it('property should is_done', function () {
       expect(shoppingListItem.is_done).to.equal(param3);
     });
   });
 
-  describe('Check', function(){
-    it('check should be a function', function(){
+  describe('Check', function () {
+    it('check should be a function', function () {
       expect(shoppingListItem.check).to.be.a('function');
     });
 
-    it('check sets is_done to true', function(){
+    it('check sets is_done to true', function () {
       shoppingListItem.check();
-      expect(shoppingListItem.is_done).to.equal(true); 
+      expect(shoppingListItem.is_done).to.equal(true);
     });
-    it('uncheck should be a function', function(){
+    it('uncheck should be a function', function () {
       expect(shoppingListItem.uncheck).to.be.a('function');
     });
-    it('uncheck sets is_done to false', function(){
+    it('uncheck sets is_done to false', function () {
       shoppingListItem.uncheck();
-      expect(shoppingListItem.is_done).to.equal(false); 
+      expect(shoppingListItem.is_done).to.equal(false);
     });
 
-    it('render should be a method', function(){
+ 
+  });
+
+  describe('Uncheck',function(){
+    it('render should be a method', function () {
       expect(shoppingListItem.render).to.be.a('function');
-     
-      
+
+
     });
-   
-    
-    it('checks if render returns a string', function(){
+
+
+    it('checks if render returns a string', function () {
       shoppingListItem.render();
       expect(shoppingListItem.render()).to.be.a('string');
 
-  });
-  it('checks if render returns a <li>', function(){
-    shoppingListItem.render();
-    expect(shoppingListItem.render()).to.equal(`<li class="completed_${param3}"><span>${param1}</span> <span>${param2}</span></li>`)
-    console.log(shoppingListItem);
+    });
+    it('checks if render returns a <li> tag', function () {
+      shoppingListItem.render();
+      expect(shoppingListItem.render()).to.equal(`<li class="completed_${param3}"><span>${param1}</span> <span>${param2}</span></li>`)
+      console.log(shoppingListItem);
+
+    });
+  })
 
 });
-});
 
-});
+describe('ShoppingList',function(){
+  let shoppingList;
+
+    beforeEach(function(){
+      shoppingList = new ShoppingListItem();
+    });
+
+    it('should be a class', function(){
+      ShoppingList.should.be.a('function');
+    });
+
+    it('should have property named items', function(){
+      shoppingList.items.should.equal('items');
+    });
+
+
+
+
+});//closing shoppingList;
